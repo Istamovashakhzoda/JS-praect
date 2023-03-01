b095b8715250d2ac126717a2ba9df5af7643ad59
shahzoda <istamovashahzoda04@gmail.com>
2023-03-01 17:12:45 +0300

    button

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// "use script"
+"use script"
 
-// let navMenu =document.querySelector(".navMenu");
-// let closeBtn=document.querySelector("#closeBtn");
-// let openBtn=document.querySelector("#openBtn");
+let navMenu =document.getElementsByClassName(".navMenu");
+let closeBtn=document.getElementById("#closeBtn");
+let openBtn=document.getElementById("#openBtn");
 
 
-// openBtn.addEventListener("click", open);
-// closeBtn.addEventListener("click", close);
+openBtn.addEventListener("click", open);
+closeBtn.addEventListener("click", close);
 
-// function open() {
-//   navMenu.style.display="block";
-//   navMenu.style.top=0;
-// }
+function open() {
+  navMenu.style.display="block";
+  navMenu.style.top=0;
+}
 
-// function close(){
-//   navMenu.style.top= "-100px";
-// }
+function close(){
+  navMenu.style.top= "-100px";
+}
 
 
 
@@ -73,20 +73,20 @@
 
 
 
-// // mainBtn .addEventListener("click", mainBtn);
-// // navbarList.addEventListener("click", navbarList)
-// //  let openBtn =document.getElementById("main__btn");
-// //  let closeBtn = document.getElementById("close__btn");
-// //  let main = document.getElementById("categories");
+// mainBtn .addEventListener("click", mainBtn);
+// navbarList.addEventListener("click", navbarList)
+//  let openBtn =document.getElementById("main__btn");
+//  let closeBtn = document.getElementById("close__btn");
+//  let main = document.getElementById("categories");
 
-// //  let openMenuBtn =function (){
-// //  main. display ="block";
-// //  };
+//  let openMenuBtn =function (){
+//  main. display ="block";
+//  };
 
-// //  let closeMenuBtn = function (){
-// //    main. display ="block"
-// //  };
+//  let closeMenuBtn = function (){
+//    main. display ="block"
+//  };
 
-// //  openBtn. addEventListener("click", openMenuBtn);
-// //  closeBtn .addEventListener("click", closeMenuBtn);
+//  openBtn. addEventListener("click", openMenuBtn);
+//  closeBtn .addEventListener("click", closeMenuBtn);
 
