8759e64ead31e66a19b4ec47ed54730aa72ad8c2
shahzoda <istamovashahzoda04@gmail.com>
2023-03-02 18:40:44 +0300

    responsive567

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-// // "use script"
+"use script"
 
-// // let navMenu =document.getElementsByClassName(".navMenu");
-// // let closeBtn=document.getElementById("#closeBtn");
-// // let openBtn=document.getElementById("#openBtn");
-
-
-// // openBtn.addEventListener("click", open);
-// // closeBtn.addEventListener("click", close);
-
-// // function open() {
-// //   navMenu.style.display="block";
-// //   navMenu.style.top=0;
-// // }
-
-// // function close(){
-// //   navMenu.style.top= "-100px";
-// // }
+let navMenu =document.getElementsByClassName(".navMenu");
+let closeBtn=document.getElementById("#closeBtn");
+let openBtn=document.getElementById("#openBtn");
 
 
+openBtn.addEventListener("click", open);
+closeBtn.addEventListener("click", close);
+
+function open() {
+  navMenu.style.display="block";
+  navMenu.style.top=0;
+}
+
+function close(){
+  navMenu.style.top= "-100px";
+}
 
 
 
@@ -25,49 +23,6 @@
 
 
 
-
-
-
-
-
-// // // mainBtn .addEventListener("click", mainBtn);
-// // // navbarList.addEventListener("click", navbarList)
-// // //  let openBtn =document.getElementById("main__btn");
-// // //  let closeBtn = document.getElementById("close__btn");
-// // //  let main = document.getElementById("categories");
-
-// // //  let openMenuBtn =function (){
-// // //  main. display ="block";
-// // //  };
-
-// // //  let closeMenuBtn = function (){
-// // //    main. display ="block"
-// // //  };
-
-// // //  openBtn. addEventListener("click", openMenuBtn);
-// // //  closeBtn .addEventListener("click", closeMenuBtn);
-
-// // let openBtn = document . getElementById("main__btn")
-// // let closeBtn = document. getElementById("close__btn")
-
-// // openBtn.addEventListener("click" , function(){
-// //   alert("hello")
-// //   mainright.style.backgroundcolor = "red";
-// //   maintitle.textContent = " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur";
-// //   maindesc.textContent = " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur"
-// // })
-
-// // onclick();
-
-// // openBtn.addEventListener("click", onclick)
-
-// // let openBtn = function(){
-// //   mainMore.style.display = "block";
-// // };
-
-// // let closeBtn = function (){
-// //   mainMore .style.display= "none";
-// // };
 
 // //  openBtn.addEventListener("click", openMenuBtn);
 // //  closeBtn.addEventListener("click",closeMenuBtn);
@@ -91,6 +46,30 @@
 // //  };
 
 // //  toogleBtn.addEventListener("click",toogleBtnFunc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const hamburger = document.getElementsByClassName(".hamburger");
