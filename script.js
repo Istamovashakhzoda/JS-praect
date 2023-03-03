d8c9ff5d2bd31075f98c3a076fd94bf98439b313
shahzoda <istamovashahzoda04@gmail.com>
2023-03-03 15:15:21 +0300

    btn2

--- script.js
+++ script.js
@@ -1,41 +1,95 @@
 "use script"
+ 
 
-let mainleft =document.querySelector(".main-left");
-let navMenu=document.querySelector(".navMenu");
-let back = document.querySelector(".back")
-let openBtn = document.getElementById(".openBtn");
-let closeBtn = document.getElementById(".closebtn")
+const openbtn = document.getElementById("openBtn");
+const closebtn = document.getElementById("closebtn");
+const openMenu = document.getElementById("button");
 
-let openMenuBtn = function (){
-  back.style.display = "block"
+
+let openButton = function(){
+  openMenu.style.display = "block"
 };
 
-let closeMenuBtn = function (){
-  back.style.display = "none";
+openbtn.addEventListener("click", openButton);
+
+let closeButton = function(){
+  openMenu.style.display = "none"
 };
 
-openBtn.addEventListener("click", openMenuBtn);
-closeBtn.addEventListener("click",closeMenuBtn)
+closebtn.addEventListener("click" , closeButton);
 
 
 
-// let mainTitle = document.querySelector(".main__left--title");
-// let mainDesc = document.querySelector(".main__left--desc");
-// let mainMore = document.querySelector(".main__more");
-
-// let mainRight = document.querySelector(".main__right");
-
-// let openBtn = document.getElementById("main__btn");
-// let closeBtn = document.getElementById("close__btn");
 
 
-// let openMenuBtn = function(){
-//   mainMore.style.display = "block"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let mainleft =document.querySelector(".main-left");
+// let navMenu=document.querySelector(".navMenu");
+// let back = document.querySelector(".back")
+// let openBtn = document.getElementById(".openBtn");
+// let closeBtn = document.getElementById(".closebtn")
+
+// let openMenuBtn = function (){
+//   back.style.display = "block"
 // };
 
 // let closeMenuBtn = function (){
-//   mainMore.style.display = "none"
+//   back.style.display = "none";
 // };
 
 // openBtn.addEventListener("click", openMenuBtn);
 // closeBtn.addEventListener("click",closeMenuBtn)
+
+
+
+// // let mainTitle = document.querySelector(".main__left--title");
+// // let mainDesc = document.querySelector(".main__left--desc");
+// // let mainMore = document.querySelector(".main__more");
+
+// // let mainRight = document.querySelector(".main__right");
+
+// // let openBtn = document.getElementById("main__btn");
+// // let closeBtn = document.getElementById("close__btn");
+
+
+// // let openMenuBtn = function(){
+// //   mainMore.style.display = "block"
+// // };
+
+// // let closeMenuBtn = function (){
+// //   mainMore.style.display = "none"
+// // };
+
+// // openBtn.addEventListener("click", openMenuBtn);
+// // closeBtn.addEventListener("click",closeMenuBtn)
